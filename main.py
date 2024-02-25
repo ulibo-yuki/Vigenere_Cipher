6ce0a4b80a34206419ad69773bead56c6914143a
yuki <fu08yu20ko07@icloud.com>
2024-02-25 16:06:09 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def encryption(key,plaintext):
+    df = pd.read_csv('Vigenere_square.csv', index_col=0)
+    row_label = str(key)
+    column_label = str(plaintext)
+    jene = df.loc[row_label, column_label]
+    return jene
+    # print(f"key{key}")
+    # print(f"text{plaintext}")
+    # print()
+
+def main():
+    print("鍵を入力してください。")
+    input_key = list(input().upper()) #ARM
+    print("平文を入力してください。")
+    input_plaintext = list(input().upper()) #HELLO
+
+    key_len = len(input_key)
+    plaintext_len = len(input_plaintext)
+
+    n = 0
+    result_list = []
+    for i in range(plaintext_len):
+        arg_key = input_key[n]
+        arg_plaintext = input_plaintext[i]
+        result_list.append(encryption(arg_key, arg_plaintext))
+        # encryption(arg_key, arg_plaintext)
+        n += 1
+        if n == key_len:
+            n = 0
+    result_str = ''.join(result_list)
+    print(result_str)
+if __name__ == "__main__":
+        main()
